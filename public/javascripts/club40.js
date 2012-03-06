7127d16106d0bbc2d1c82274037fa10fcc2b878d
Matt Hisamoto <mwhisamoto@gmail.com>
2012-03-06 13:21:42 -0500

    done ish

--- public/javascripts/club40.js
+++ public/javascripts/club40.js
@@ -1,35 +1,13 @@
 var List = {
    
   setup: function() {
-	
-	var jsp_settings = {
-	  maintainPosition: true,
-	  stickToBottom: true,
-	  animateScroll: true,
-	  animateDuration: 200,
-	  animateEase: "swing",
-	  verticalDragMinHeight: 35,
-	  verticalDragMaxHeight: 150
-	}
-	
-	if( List.apply() ) {
-	  List.sp = $('#user_list').jScrollPane(jsp_settings);
-	  List.pane = List.sp.data('jsp');
-    }
 
-    $(window).resize(function() {
-	  List.pane.reinitialise();
-	});
-	
 	$('form').die().live('submit', function(e) {
 	  var name  = $.trim($('.user_name').val())
-	  var loc   = $.trim($('.user_location').val())
-	  var story = $.trim($('.user_story').val())
-	  if( name.length > 0 && loc.length > 0 && name.toLowerCase() != "NAME ( FIRST, LAST INITIAL )".toLowerCase() && loc.toLowerCase() != "LOCATION ( CITY, STATE )".toLowerCase()) {
-		if(story.toLowerCase() == "SHARE YOUR STORY\n( IT'S OKAY TO LEAVE STUFF OUT )".toLowerCase()) $('.user_story').val("");
-		if(loc.toLowerCase() == "LOCATION ( CITY, STATE )".toLowerCase()) $('.user_location').val("");
+	  var wish = $.trim($('.user_wish').val())
+	  if( name.length > 0 && wish.length > 0 && name.toLowerCase() != "Name".toLowerCase() && wish.toLowerCase() != "Wish".toLowerCase()) {
 	  } else {
-		alert("Please fill out Name and Location fields.")
+		alert("Please fill out both fields.")
 		return false;
 	  }
 	})
@@ -57,29 +35,8 @@ var List = {
 		elem.unbind("focus");
 	  })
 	})
-	
-  },
-
-  apply: function() {
-	$(".user").each( function() {
-	  var user = $(this);
-	  user.hover( function() {
-		List.expand(user);
-	  }, function() {
-		List.contract(user);
-	  });
-	});
-	return true;
-  },
-   
-  expand: function(user) {
-	user.find(".story").removeClass("hidden").hide().show(200,"linear", function() { List.pane.reinitialise(); } );
-  },
-
-  contract: function(user) {
-    user.find(".story").hide(200, "linear", function() { user.find(".story").addClass("hidden"); List.pane.reinitialise(); } );
-  },
-	
+		
+  }
 }
 
 List.setup();
