5814178f8248f0b3092f382fb784ab79da6249f2
Matt Hisamoto <mwhisamoto@gmail.com>
2012-03-06 13:23:12 -0500

    updated deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
-set :application, "norovirus"
-set :repository,  "git@github.com:matthisamoto/norovirus.git"
+set :application, "club40"
+set :repository,  "git@github.com:matthisamoto/club40.git"
 set :default_environment, { 
   'PATH' => "/home/deploy/.rvm/rubies/ruby-1.9.2-p180/bin:/home/deploy/.rvm/gems/ruby-1.9.2-p180/bin:/home/deploy/.rvm/bin:$PATH",
   'RUBY_VERSION' => 'ruby-1.9.2-p180',
@@ -10,14 +10,14 @@ set :default_environment, {
 
 set :scm, :git
 set :branch, 'master'
-set :user, 'deploy'
+set :user, 'matt'
 
 set :use_sudo, false
-set :deploy_to, "/home/deploy/sites/norovirus"
+set :deploy_to, "/home/deploy/sites/club40"
 set :deploy_via, :copy
-set :copy_remote_dir, "/home/deploy/sites/norovirus/tmp"
+set :copy_remote_dir, "/home/deploy/sites/club40/tmp"
 set :copy_exclude, [".git"]
 
-role :web, '174.143.146.148'                          # Your HTTP server, Apache/etc
-role :app, '174.143.146.148'                          # This may be the same as your `Web` server
-role :db,  '174.143.146.148', :primary => true        # This is where Rails migrations will run
+role :web, 'club40girl.com'                          # Your HTTP server, Apache/etc
+role :app, 'club40girl.com'                          # This may be the same as your `Web` server
+role :db,  'club40girl.com', :primary => true        # This is where Rails migrations will run
