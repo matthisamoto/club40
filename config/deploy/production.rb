de36907f69a9b79615059ec0fb4ff1cc1b48ebc5
Matt Hisamoto <mwhisamoto@gmail.com>
2012-03-06 13:25:46 -0500

    updated deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,11 @@
 set :application, "club40"
 set :repository,  "git@github.com:matthisamoto/club40.git"
 set :default_environment, { 
-  'PATH' => "/home/deploy/.rvm/rubies/ruby-1.9.2-p180/bin:/home/deploy/.rvm/gems/ruby-1.9.2-p180/bin:/home/deploy/.rvm/bin:$PATH",
+  'PATH' => "/home/matt/.rvm/rubies/ruby-1.9.2-p180/bin:/home/matt/.rvm/gems/ruby-1.9.2-p180/bin:/home/matt/.rvm/bin:$PATH",
   'RUBY_VERSION' => 'ruby-1.9.2-p180',
-  'GEM_HOME' => '/home/deploy/.rvm/gems/ruby-1.9.2-p180/',
-  'GEM_PATH' => '/home/deploy/.rvm/gems/ruby-1.9.2-p180/',
-  'BUNDLE_PATH' => '/home/deploy/.rvm/gems/ruby-1.9.2-p180/'
+  'GEM_HOME' => '/home/matt/.rvm/gems/ruby-1.9.2-p180/',
+  'GEM_PATH' => '/home/matt/.rvm/gems/ruby-1.9.2-p180/',
+  'BUNDLE_PATH' => '/home/matt/.rvm/gems/ruby-1.9.2-p180/'
 }
 
 set :scm, :git
@@ -13,9 +13,9 @@ set :branch, 'master'
 set :user, 'matt'
 
 set :use_sudo, false
-set :deploy_to, "/home/deploy/sites/club40"
+set :deploy_to, "/home/matt/sites/club40"
 set :deploy_via, :copy
-set :copy_remote_dir, "/home/deploy/sites/club40/tmp"
+set :copy_remote_dir, "/home/matt/sites/club40/tmp"
 set :copy_exclude, [".git"]
 
 role :web, 'club40girl.com'                          # Your HTTP server, Apache/etc
